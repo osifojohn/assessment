712c74127b512728e1086b89a062a7999f694a2b
Osifo John <osifojohntec@gmail.com>
2023-12-09 12:11:48 +0100

    all sectors fetched at once

--- client/src/useFetch.ts
+++ client/src/useFetch.ts
@@ -6,27 +6,18 @@ const useFetchSectorsFormData = (isEditing: boolean) => {
   const [other, setOther] = useState({});
   const [service, setService] = useState({});
 
-  const manufacturingApi = `https://task-w3z1.onrender.com/sectors/manufacturing`;
-  const serviceApi = `https://task-w3z1.onrender.com/sectors/service`;
-  const otherApi = 'https://task-w3z1.onrender.com/sectors/other';
-
-  const getManufacturing = axios.get(manufacturingApi);
-  const getService = axios.get(serviceApi);
-  const getOther = axios.get(otherApi);
-
-  const fetchData = () => {
-    axios
-      .all([getManufacturing, getOther, getService])
-      .then(
-        axios.spread((manufacturingData, otherData, serviceData) => {
-          setManufacturing(manufacturingData.data);
-          setOther(otherData.data);
-          setService(serviceData.data);
-        })
-      )
-      .catch((error) => {
-        console.error('Error fetching sectors:', error);
-      });
+  const fetchData = async () => {
+    try {
+      const apiUrl = 'https://task-w3z1.onrender.com/sectors/all-sectors';
+      const { data } = await axios.get(apiUrl);
+      if (data) {
+        setManufacturing(data.manufacturing);
+        setOther(data.other);
+        setService(data.service);
+      }
+    } catch (error) {
+      console.error('Error fetching sectors:', error);
+    }
   };
 
   useEffect(() => {
