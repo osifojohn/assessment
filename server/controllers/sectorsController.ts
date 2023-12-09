9fdd3ffcf4ac195aaccfae831ef697ee9a43c25e
Osifo John <osifojohntec@gmail.com>
2023-12-09 08:25:22 +0100

    modified sectors data backend

--- server/controllers/sectorsController.ts
+++ server/controllers/sectorsController.ts
@@ -3,7 +3,12 @@ import asyncHandler from 'express-async-handler';
 import { OtherSector } from '../models/sectors/other';
 import { STATUSCODE } from '../types';
 import { ServicesSector } from '../models/sectors/service';
-import { sectorsData } from '../sectors';
+import {
+  manufacturingSector,
+  otherSector,
+  sectorsData,
+  serviceSector,
+} from '../sectors';
 import { ManufacturingSector } from '../models/sectors/manufacturing';
 
 const MANUFACTURING_SECTOR_ID = '657371df88ff5e105c937055';
@@ -28,9 +33,9 @@ export const addOtherSector = asyncHandler(
 
 export const getOtherSector = asyncHandler(
   async (req: Request, res: Response) => {
-    const other = await OtherSector.findById(OTHER_SECTOR_ID);
-
-    res.json(other);
+    // const other = await OtherSector.findById(OTHER_SECTOR_ID);
+    const otherSectorData = otherSector;
+    res.json(otherSectorData);
   }
 );
 
@@ -47,9 +52,9 @@ export const addServiceSector = asyncHandler(
 
 export const getServiceSector = asyncHandler(
   async (req: Request, res: Response) => {
-    const service = await ServicesSector.findById(SERVICE_SECTOR_ID);
-
-    res.json(service);
+    // const service = await ServicesSector.findById(SERVICE_SECTOR_ID);
+    const serviceSectorData = serviceSector;
+    res.json(serviceSectorData);
   }
 );
 
@@ -70,10 +75,12 @@ export const addManufacturingSector = asyncHandler(
 
 export const getManufacturingSector = asyncHandler(
   async (req: Request, res: Response) => {
-    const manufacturing = await ManufacturingSector.findById(
-      MANUFACTURING_SECTOR_ID
-    );
+    // const manufacturing = await ManufacturingSector.findById(
+    //   MANUFACTURING_SECTOR_ID
+    // );
 
-    res.json(manufacturing);
+    const manufacturingSectorData = manufacturingSector;
+
+    res.json(manufacturingSectorData);
   }
 );
